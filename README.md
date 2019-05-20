a4f6b423257df3669c7a989b48abb0d8facd44a8
Awpteamoose <awpteamoose@gmail.com>
2019-05-21 02:11:10 +0300

    generalized away form amethyst state, more friendly api

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# state_machine
-
-amethyst.rs state machine generator macro.
-Made exactly for abstracting over `update/handle_events` method parameters.
-
-## Usage:
-`state_machine!(StateMachine; State; _world: &mut World, _something: &mut f64);`
-
-Will generate StateMachine struct and trait for using with State trait implementations all methods of which should take `&mut World` and `&mut f64` as parameters.
-
-Or something like this:
-`
+```rust
 state_machine!(
-    PlayerStateMachine; 
-    PlayerState; 
-    _mv: &mut MovingObject,
-    _bb: &mut HasAABB,
-    _anim: &mut HasAnimationSequence,
-    _rend: &mut Renderable,
-    _dir: &Directional,
-    _pi: &PlayerInput,
-    _delta: &DeltaTime
+    player;
+    GetHit, Jump;
+    mv: &mut MovingObject,
+    bb: &mut HasAABB,
+    anim: &mut HasAnimationSequence,
+    rend: &mut Renderable,
+    dir: &Directional,
+    pi: &PlayerInput,
+    delta: &DeltaTime
 );
-`
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,237 +1,140 @@
-// #![feature(type_ascription)]
-
 #[macro_export]
 macro_rules! state_machine {
-    ( $sm: ident; $sn: ident; $($n: ident: $t: ty),* ) => {
-        pub enum Trans {
-            None,
-            Pop,
-            Push(Box<$sn>),
-            Switch(Box<$sn>),
-            Quit,
-        }
+	($name: ident; $($event: ident),+$(,)*; $($n: ident: $t: ty),*$(,)*) => {
+		pub mod $name {
+			pub enum Trans {
+				None,
+				Pop,
+				Push(Box<dyn State>),
+				Switch(Box<dyn State>),
+				Quit,
+			}
 
-        pub trait $sn {
-            fn on_start(&mut self, $($n: $t),*) {}
-            fn on_stop(&mut self, $($n: $t),*) {}
-            fn on_pause(&mut self, $($n: $t),*) {}
-            fn on_resume(&mut self, $($n: $t),*) {}
-            /// Executed on every frame before updating, for use in reacting to events.
-            fn handle_events(&mut self, $($n: $t),*) -> Trans {
-                Trans::None
-            }
+			impl Trans {
+				pub fn none() -> Self { Trans::None }
+				pub fn pop() -> Self { Trans::Pop }
+				pub fn quit() -> Self { Trans::Quit }
+				pub fn push(state: impl State) -> Self { Trans::Push(Box::new(state)) }
+				pub fn switch(state: impl State) -> Self { Trans::Switch(Box::new(state)) }
+			}
 
-            /// Executed repeatedly at stable, predictable intervals (1/60th of a second
-            /// by default).
-            fn fixed_update(&mut self, $($n: $t),*) -> Trans {
-                Trans::None
-            }
+			pub trait State: Send + Sync + 'static {
+				fn on_start(&mut self, $($n: $t,)*) {}
+				fn on_stop(&mut self, $($n: $t,)*) {}
+				fn on_pause(&mut self, $($n: $t,)*) {}
+				fn on_resume(&mut self, $($n: $t,)*) {}
 
-            /// Executed on every frame immediately, as fast as the engine will allow.
-            fn update(&mut self, $($n: $t),*) -> Trans {
-                Trans::None
-            }
-        }
+				fn update(&mut self, $($n: $t,)*) -> Trans { Trans::None }
+				fn trigger(&mut self, event: Event, $($n: $t,)*) -> Trans { Trans::None }
+			}
 
-        unsafe impl Sync for $sm {}
-        unsafe impl Send for $sm {}
+			pub enum Event {
+				$($event,)+
+			}
 
-        pub struct $sm {
-            running: bool,
-            state_stack: Vec<Box<$sn>>,
-        }
+			pub struct StateMachine {
+				pub running: bool,
+				pub state_stack: Vec<Box<dyn State>>,
+			}
 
-        impl $sm {
-            pub fn new<S>(initial_state: S) -> $sm
-            where
-                S: $sn + 'static, {
-                $sm {
-                    running: false,
-                    state_stack: vec![Box::new(initial_state)],
-                }
-            }
+			impl StateMachine {
+				pub fn new(initial_state: impl State) -> Self {
+					Self {
+						running: false,
+						state_stack: vec![Box::new(initial_state)],
+					}
+				}
 
-            pub fn is_running(&self) -> bool {
-                self.running
-            }
+				pub fn start(&mut self, $($n: $t,)*) {
+					if !self.running {
+						let state = self.state_stack.last_mut().unwrap();
+						state.on_start($($n,)*);
+						self.running = true;
+					}
+				}
 
-            pub fn start(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if !self.running {
-                    let state = self.state_stack.last_mut().unwrap();
-                    state.on_start($($n),*);
-                    self.running = true;
-                }
-            }
+				pub fn update(&mut self, $($n: $t,)*) {
+					let trans = if let Some(state) = self.state_stack.last_mut() {
+						state.update($($n,)*)
+					} else {
+						Trans::None
+					};
+					self.transition(trans, $($n,)*);
+				}
 
-            pub fn handle_events(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    let trans = match self.state_stack.last_mut() {
-                        Some(state) => state.handle_events($($n),*),
-                        None => Trans::None,
-                    };
+				fn transition(&mut self, request: Trans, $($n: $t,)*) {
+					if self.running {
+						match request {
+							Trans::None => (),
+							Trans::Pop => self.pop($($n,)*),
+							Trans::Push(state) => self.push(state, $($n,)*),
+							Trans::Switch(state) => self.switch(state, $($n,)*),
+							Trans::Quit => self.stop($($n,)*),
+						}
+					}
+				}
 
-                    self.transition(trans, $($n),*);
-                }
-            }
+				pub fn switch(&mut self, mut state: Box<dyn State>, $($n: $t,)*) {
+					if self.running {
+						if let Some(mut state) = self.state_stack.pop() {
+							state.on_stop($($n,)*)
+						}
 
-            pub fn fixed_update(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    let trans = match self.state_stack.last_mut() {
-                        Some(state) => state.fixed_update($($n),*),
-                        None => Trans::None,
-                    };
+						state.on_start($($n,)*);
+						self.state_stack.push(state);
+					}
+				}
 
-                    self.transition(trans, $($n),*);
-                }
-            }
+				pub fn push(&mut self, mut state: Box<dyn State>, $($n: $t,)*) {
+					if self.running {
+						if let Some(state) = self.state_stack.last_mut() {
+							state.on_pause($($n,)*);
+						}
 
-            pub fn update(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    let trans = match self.state_stack.last_mut() {
-                        Some(state) => state.update($($n),*),
-                        None => Trans::None,
-                    };
+						state.on_start($($n,)*);
+						self.state_stack.push(state);
+					}
+				}
 
-                    self.transition(trans, $($n),*);
-                }
-            }
+				pub fn pop(&mut self, $($n: $t,)*) {
+					if self.running {
+						if let Some(mut state) = self.state_stack.pop() {
+							state.on_stop($($n,)*);
+						}
 
-            fn transition(
-                &mut self,
-                request: Trans,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    match request {
-                        Trans::None => (),
-                        Trans::Pop => self.pop($($n),*),
-                        Trans::Push(state) => self.push(state, $($n),*),
-                        Trans::Switch(state) => self.switch(state, $($n),*),
-                        Trans::Quit => self.stop($($n),*),
-                    }
-                }
-            }
+						if let Some(state) = self.state_stack.last_mut() {
+							state.on_resume($($n,)*);
+						} else {
+							self.running = false;
+						}
+					}
+				}
 
-            fn switch(
-                &mut self,
-                state: Box<$sn>,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    if let Some(mut state) = self.state_stack.pop() {
-                        state.on_stop($($n),*)
-                    }
+				pub fn stop(&mut self, $($n: $t,)*) {
+					if self.running {
+						while let Some(mut state) = self.state_stack.pop() {
+							state.on_stop($($n,)*);
+						}
 
-                    self.state_stack.push(state);
-                    let state = self.state_stack.last_mut().unwrap();
-                    state.on_start($($n),*);
-                }
-            }
+						self.running = false;
+					}
+				}
 
-            fn push(
-                &mut self,
-                state: Box<$sn>,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    if let Some(state) = self.state_stack.last_mut() {
-                        state.on_pause($($n),*);
-                    }
+				pub fn is_state<T: State>(&self) -> bool {
+					std::any::Any::is::<T>(&self.state_stack[0])
+				}
 
-                    self.state_stack.push(state);
-                    let state = self.state_stack.last_mut().unwrap();
-                    state.on_start($($n),*);
-                }
-            }
-
-            fn pop(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    if let Some(mut state) = self.state_stack.pop() {
-                        state.on_stop($($n),*);
-                    }
-
-                    if let Some(state) = self.state_stack.last_mut() {
-                        state.on_resume($($n),*);
-                    } else {
-                        self.running = false;
-                    }
-                }
-            }
-
-            fn stop(
-                &mut self,
-                $($n: $t),*
-            ) {
-                if self.running {
-                    while let Some(mut state) = self.state_stack.pop() {
-                        state.on_stop($($n),*);
-                    }
-
-                    self.running = false;
-                }
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-state_machine!(TestStateMachine; TestState; _a: &mut isize, _b: isize);
-
-#[cfg(test)]
-pub struct Test;
-
-#[cfg(test)]
-impl TestState for Test {
-    fn on_start(&mut self, a: &mut isize, b: isize) {
-        *a += b;
-    }
-
-    fn on_resume(&mut self, a: &mut isize, b: isize) {
-        self.on_start(a, b);
-    }
-
-    fn update(&mut self, _a: &mut isize, _b: isize) -> Trans {
-        Trans::Push(Box::new(Test))
-    }
-
-    fn fixed_update(&mut self, _a: &mut isize, _b: isize) -> Trans {
-        Trans::Pop
-    }
-}
-
-#[test]
-fn sm_test() {
-    let mut sm = TestStateMachine::new(Test);
-
-    let mut a = 0;
-    let b = 10;
-
-    sm.start(&mut a, b);
-    assert!(a == 10);
-
-    sm.update(&mut a, b);
-    assert!(a == 20);
-
-    sm.fixed_update(&mut a, b);
-    assert!(a == 30);
-
-    sm.fixed_update(&mut a, b);
-    assert!(a == 30);
-
-    assert!(!sm.is_running())
+				pub fn trigger(&mut self, event: Event, $($n: $t,)*) {
+					if self.running {
+						let trans = if let Some(state) = self.state_stack.last_mut() {
+							state.trigger(event, $($n,)*)
+						} else {
+							Trans::None
+						};
+						self.transition(trans, $($n,)*);
+					}
+				}
+			}
+		}
+	};
 }
